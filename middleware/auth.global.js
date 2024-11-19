24dbe79e1ee46eadb65ab7a9d4d7b4cd43e0c20b
ogabeksanoyev06 <ogabeksanoyev06@.com>
2024-11-19 17:32:29 +0500

    .

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -4,7 +4,11 @@ export default defineNuxtRouteMiddleware((to, from) => {
 
   const publicPaths = [localePath('/auth/login'), localePath('/auth/register')]
 
-  if (!accessTokenCookie.value && !publicPaths.includes(to.path)) {
+  const exceptionPaths = [localePath('/auth/reset-password/'), localePath('/confirmEmail/')]
+
+  const isException = exceptionPaths.some((path) => to.path.startsWith(path))
+
+  if (!accessTokenCookie.value && !publicPaths.includes(to.path) && !isException) {
     return navigateTo(localePath('/auth/login'))
   }
 
