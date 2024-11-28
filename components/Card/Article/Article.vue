b8c69a166823e314452c14653b591063b8d96b40
ogabeksanoyev06 <ogabeksanoyev06@.com>
2024-11-28 13:24:57 +0500

    .

--- components/Card/Article/Article.vue
+++ components/Card/Article/Article.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="rounded-2xl bg-white border border-gray-4 p-4 md:p-6 flex flex-col items-start transition-300 hover:shadow-card h-full">
-    <NuxtLink :to="localePath(`/journal/${route.params.journalSlug}/${article.id}`)" class="leading-150 text-neutral mb-2">
-      <span v-for="(collaborator, index) in article?.collaborators" :key="index"> {{ collaborator }}<span v-if="index < article.collaborators?.length - 1">, </span> </span>
+    <NuxtLink class="flex-1 flex flex-col w-full" :to="localePath(`/journal/${route.params.journalSlug}/${article.id}`)">
+      <div class="leading-150 text-neutral mb-2">
+        <span v-for="(collaborator, index) in article?.collaborators" :key="index"> {{ collaborator }}<span v-if="index < article.collaborators?.length - 1">, </span> </span>
+      </div>
+      <h3 class="text-lg md:text-xl text-secondary leading-140 font-semibold mb-4">
+        {{ article.title }}
+      </h3>
+      <div class="md:text-base text-dark leading-140 line-clamp-4" v-html="article.abstractInfo"></div>
+      <div class="flex-center-between max-md:!flex-col gap-3 w-full mt-6">
+        <ul class="flex items-center gap-4 flex-wrap">
+          <li class="flex-y-center gap-1 text-secondary font-medium text-base">
+            <span class="icon-file text-xl leading-5"></span>
+            {{ article.pages }}
+          </li>
+          <hr class="w-px h-5 bg-gray-4 shrink-0" />
+          <li class="flex-y-center gap-1 text-secondary font-medium text-base">
+            <span class="icon-eye text-xl leading-5"></span>
+            {{ article.views }}
+          </li>
+          <hr class="w-px h-5 bg-gray-4 shrink-0" />
+          <li class="flex-y-center gap-1 text-secondary font-medium text-base">
+            <span class="icon-download text-xl leading-5"></span>
+            {{ article.downloads }}
+          </li>
+        </ul>
+      </div>
     </NuxtLink>
-    <h3 class="text-lg md:text-xl text-secondary leading-140 font-semibold mb-4">
-      {{ article.title }}
-    </h3>
-    <div class="md:text-base text-dark leading-140 line-clamp-4" v-html="article.abstractInfo"></div>
-    <div class="flex-center-between max-md:!flex-col gap-3 w-full mt-6">
-      <ul class="flex items-center gap-4 flex-wrap">
-        <li class="flex-y-center gap-1 text-secondary font-medium text-base">
-          <span class="icon-file text-xl leading-5"></span>
-          {{ article.pages }}
-        </li>
-        <hr class="w-px h-5 bg-gray-4 shrink-0" />
-        <li class="flex-y-center gap-1 text-secondary font-medium text-base">
-          <span class="icon-eye text-xl leading-5"></span>
-          {{ article.views }}
-        </li>
-        <hr class="w-px h-5 bg-gray-4 shrink-0" />
-        <li class="flex-y-center gap-1 text-secondary font-medium text-base">
-          <span class="icon-download text-xl leading-5"></span>
-          {{ article.downloads }}
-        </li>
-      </ul>
-
+    <div class="flex md:justify-end gap-3 w-full mt-4">
       <UIButton @click="downloadAllFiles(article.id, article.files)" :loading :text="translations['main.upload-article']" icon="icon-download text-xl leading-5" wrapper-class="max-md:w-full"></UIButton>
     </div>
   </div>
